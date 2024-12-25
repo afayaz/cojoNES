c3f986bfd27eb4c39ad5c5252f0324ceae724209
Ahsan Fayaz <ahsan@ahsanfayaz.com>
2024-12-25 20:26:03 +0000

    Add SDL3 and Dear Imgui as dependencies
    For now, these will be used for a visual display of the state of the CPU
    and memory to help debugging.
    
    I plan on using SDL3 for input, video and sound. However, I'm not too
    keen on using Dear Imgui as the main UI so I'll pick something else when
    the time comes.
    
    Note that Dependencies.cmake has to specify everything related to SDL3
    and Imgui in a specific order. Imgui doesn't have its own CMake file,
    and so we have to create the target and then specify the header include
    path for SDL3 to ensure that Imgui's SDL3 backend can build.

--- Dependencies.cmake
+++ Dependencies.cmake
@@ -8,9 +8,44 @@ function(cojoNES_setup_dependencies)
   # For each dependency, see if it's
   # already been provided to us by a parent project
 
+  if(NOT TARGET Catch2::Catch2WithMain)
+    CPMAddPackage("gh:catchorg/Catch2@3.3.2")
+  endif()
+
   if(NOT TARGET fmtlib::fmtlib)
     CPMAddPackage("gh:fmtlib/fmt#9.1.0")
   endif()
+
+  # SDL3 and Imgui must be in this order to ensure Imgui's SDL3 backend can see
+  # the SDL3 header include paths. Imgui suggests that we build the specific
+  # backends as part of our own project, but I think this is much cleaner.
+  # Will revisit if we need multiple backends and causes things to get messy.
+  if(NOT TARGET SDL3::SDL3)
+    CPMAddPackage(
+        NAME SDL3
+        GITHUB_REPOSITORY libsdl-org/SDL
+        GIT_TAG preview-3.1.6
+        OPTIONS
+        "BUILD_SHARED_LIBS ON"
+        "SDL_DISABLE_INSTALL ON"
+        "SDL_STATIC_PIC ON"
+        "SDL_WERROR OFF"
+        "SDL_LIBC ON")
+  endif()
+
+  if(NOT TARGET imgui)
+    CPMAddPackage(
+        NAME imgui
+        GITHUB_REPOSITORY ocornut/imgui
+        GIT_TAG v1.91.6)
+  endif()
+
+  add_library(imgui ${imgui_SOURCE_DIR}/imgui.cpp ${imgui_SOURCE_DIR}/imgui_demo.cpp ${imgui_SOURCE_DIR}/imgui_draw.cpp ${imgui_SOURCE_DIR}/imgui_tables.cpp ${imgui_SOURCE_DIR}/imgui_widgets.cpp ${imgui_SOURCE_DIR}/backends/imgui_impl_sdl3.cpp ${imgui_SOURCE_DIR}/backends/imgui_impl_sdlrenderer3.cpp)
+
+  target_include_directories(imgui SYSTEM PUBLIC ${SDL3_SOURCE_DIR}/include)
+  include_directories(${imgui_SOURCE_DIR} ${imgui_SOURCE_DIR}/backends)
+
+  include_directories(${SDL3_INCLUDE_DIR})
 
   if(NOT TARGET spdlog::spdlog)
     CPMAddPackage(
@@ -22,10 +57,6 @@ function(cojoNES_setup_dependencies)
       "gabime/spdlog"
       OPTIONS
       "SPDLOG_FMT_EXTERNAL ON")
-  endif()
-
-  if(NOT TARGET Catch2::Catch2WithMain)
-    CPMAddPackage("gh:catchorg/Catch2@3.3.2")
   endif()
 
 endfunction()
