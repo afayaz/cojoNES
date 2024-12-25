ca1e19156f02155e86db9f852cb171a0d3ce6edf
Ahsan Fayaz <ahsan@ahsanfayaz.com>
2024-12-25 20:54:06 +0000

    Remove logging from memory read and write
    Now that we have a debug UI for the memory layout, this isn't needed.
    The memory view goes via the system bus, and so logging 256+ times per
    frame was slowing things down an awful lot.

--- source/Memory.hpp
+++ source/Memory.hpp
@@ -2,31 +2,11 @@
 
 #include <cstdint>
 
-#include <fmt/core.h>
-#include <spdlog/spdlog.h>
-
 class Memory
 {
 public:
-	uint8_t Read(uint16_t address) { SPDLOG_INFO("Read {:#04x} from address {:#06x}", a[address], address); return a[address]; }
-	void    Write(uint16_t address, uint8_t data)
-	{
-		SPDLOG_INFO("Write {:#04x} to address {:#06x}", data, address);
-		
-		a[address] = data;
-		
-		SPDLOG_INFO("Surrounding bytes of memory:");
-
-		uint16_t base = address & 0xFFF0;
-
-		std::string debugMemBytes = fmt::format("{:#06x}: ", base);
-
-		for (size_t i = base; i < base + 0x0F; ++i)
-		{
-			debugMemBytes += fmt::format("{:#04x} ", a[i]);
-		}
-		SPDLOG_INFO(debugMemBytes);
-	}
+	uint8_t Read(uint16_t address) { return a[address]; }
+	void    Write(uint16_t address, uint8_t data) { a[address] = data; }
 
 private:
 	uint8_t a[0xFFFF];
